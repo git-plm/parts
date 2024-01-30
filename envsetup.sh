24b4ca140aa39a7b83648f4621871c5e52b6d182
Cliff Brake <cbrake@bec-systems.com>
2024-01-30 15:14:56 -0500

    add more parts

--- envsetup.sh
+++ envsetup.sh
@@ -2,7 +2,7 @@ parts_db_create() {
 	rm parts.sqlite
 
 	# define all of your libs here -- should be a CSV file for each lib
-	libs="res cap"
+	libs="res cap con mpu"
 
 	for lib in $libs; do
 		sqlite3 --csv ./parts.sqlite ".import ${lib}.csv ${lib}" || return 1
