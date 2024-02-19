55462df43ffb1104cbff9d4dbda5892ca62f5872
Cliff Brake <cbrake@bec-systems.com>
2024-02-19 15:16:39 -0500

    more parts

--- envsetup.sh
+++ envsetup.sh
@@ -2,7 +2,7 @@ parts_db_create() {
 	rm parts.sqlite
 
 	# define all of your libs here -- should be a CSV file for each lib
-	libs="cap con cpd dio ics ind mpu pwr res reg tra"
+	libs="ana cap con cpd dio ics ind mpu pwr res reg tra"
 
 	for lib in $libs; do
 		sqlite3 --csv ./parts.sqlite ".import ${lib}.csv ${lib}" || return 1
